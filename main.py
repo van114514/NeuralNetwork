231b2c856d82596e8654715090a3080ab800e7ee
van114514 <van45450721919@gmail.com>
2020-07-16 02:20:19 +0900

    Improved generateDataSet

--- main.py
+++ main.py
@@ -1,66 +1,28 @@
 import numpy as np
-from math import floor
 import os
 import NN
 
-# def partially_feature(p, ii, jj):
-#     feature = 0.0
-#     for i in range(8):
-#         for j in range(8):
-#             if(p[8*ii + i][8*jj + j] == '1'):
-#                 feature += 1
-#     return feature / 64
 
-# def extract_feature(p):
-#     buf = np.empty(64, dtype='float32')
-#     for i in range(8):
-#         for j in range(8):
-#             buf[8*i + j] = partially_feature(p, i, j)
-#     return buf
-
-# def read_onefile(path):
-#     buf = np.empty((100,64), dtype='float32')
-#     with open(path) as f:
-#         p = np.array(f.readlines())
-#     for charas in range(100):
-#         buf[charas][:] = extract_feature(p[64*charas:64*charas+64])
-#     return buf
-
-
-
-# def generateDateSet(who,how):
-#     data = np.empty((20,100,64),dtype='float32')
-
-#     for i in range(20):
-#         buf = np.empty((100,64), dtype='float32')
-#         with open("hira{}_{:0>2}{}.dat".format(who,i,how)) as f:
-#             p = np.array(f.readlines())
-#         for charas in range(100):
-#             buf[charas][:] = extract_feature(p[64*charas:64*charas+64])
-#         data[i][:] = buf
-    
-#     return data
-
-def generateDateSet(who,how):
+def generateDataSet(who,how):
     data = np.empty((20,100,64),dtype='float32')
 
-    for i in range(20):
+    for num in range(20):
         mesh = np.zeros((100,64), dtype='float32')
-        with open("hira{}_{:0>2}{}.dat".format(who,i,how)) as f:
+        with open("hira{}_{:0>2}{}.dat".format(who,num,how)) as f:
             p = np.array(f.readlines())
-        for charas in range(100):
-            char = p[64*charas:64*charas+64]
-            for i in range(64):
-                for j in range(64):
-                    if char[i][j] == '1':
-                        mesh[charas][floor(i/8)+floor(j/8)] += 1
-        data[i][:] = mesh/64
+        for i in range(100):
+            char = p[64*i:64*i+64]
+            for j in range(64):
+                for k in range(64):
+                    if char[j][k] == '1':
+                        mesh[i][8*(j//8)+k//8] += 1
+        data[num][:] = mesh/64
     return data
 
 def experiment1():
-    writer0_L = generateDateSet(0,"L")
-    writer0_T = generateDateSet(0,"T")
-    writer1_T = generateDateSet(1,"T")
+    writer0_L = generateDataSet(0,"L")
+    writer0_T = generateDataSet(0,"T")
+    writer1_T = generateDataSet(1,"T")
 
     nn = NN.NeuralNet(64,64,20)
     nn.set_param(0.8, 0.3)
@@ -74,9 +36,9 @@ def experiment1():
     print(nn.test(writer1_T))
 
 def experiment2():
-    writer1_L = generateDateSet(1,"L")
-    writer0_T = generateDateSet(0,"T")
-    writer1_T = generateDateSet(1,"T")
+    writer1_L = generateDataSet(1,"L")
+    writer0_T = generateDataSet(0,"T")
+    writer1_T = generateDataSet(1,"T")
 
     nn = NN.NeuralNet(64,64,20)
     nn.set_param(0.8, 0.3)
@@ -90,10 +52,10 @@ def experiment2():
     print(nn.test(writer1_T))
 
 def experiment3():
-    writer0_L = generateDateSet(0,"L")
-    writer1_L = generateDateSet(1,"L")
-    writer0_T = generateDateSet(0,"T")
-    writer1_T = generateDateSet(1,"T")
+    writer0_L = generateDataSet(0,"L")
+    writer1_L = generateDataSet(1,"L")
+    writer0_T = generateDataSet(0,"T")
+    writer1_T = generateDataSet(1,"T")
 
     writer01_L = np.empty((20,200,64),dtype='float32')
     writer01_T = np.empty((20,200,64),dtype='float32')
